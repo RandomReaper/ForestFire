97970f06953dde9acfd77bb483e08db24b8fccdf
Marc Pignat <marc@pignat.org>
2022-03-09 09:21:28 +0100

    fix JS alpha rendering

--- ff/js/src/main/scala/org/pignat/forestfire/JSDrawable.scala
+++ ff/js/src/main/scala/org/pignat/forestfire/JSDrawable.scala
@@ -8,17 +8,19 @@ class JSDrawable(id: String) extends Drawable {
   val c = document.getElementById(id).asInstanceOf[html.Canvas]
   val ctx = c.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
 
+  private def color(c:Color) : String = f"rgba(${c.r},${c.g},${c.b},${c.a/255.0})"
+
   override def height(): Int = c.height
 
   override def width(): Int = c.width
 
   override def setPixel(x: Integer, y: Integer, c: Color): Unit = {
-    ctx.fillStyle = f"rgba(${c.r},${c.g},${c.b},${c.a})"
+    ctx.fillStyle = color(c)
     ctx.fillRect(x.toDouble, y.toDouble, 1, 1)
   }
 
   override def drawFilledRect(x: Integer, y: Integer, w: Integer, h: Integer, c: Color): Unit = {
-    ctx.fillStyle = f"rgba(${c.r},${c.g},${c.b},${c.a/255.0})"
+    ctx.fillStyle = color(c)
     ctx.fillRect(x.toDouble, y.toDouble, w.toDouble, h.toDouble)
   }
 }
